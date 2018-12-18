f207404f7861a3a671d870daec8f63c18c07f4f2
Ayush Gupta <39510321+ayushgupta1997@users.noreply.github.com>
2018-12-18 23:54:49 +0530

    Update (A)LIS(nlogk) .cpp

--- (A)LIS(nlogk) .cpp
+++ (A)LIS(nlogk) .cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int inf = 10000000;
 // Complete the longestIncreasingSubsequence function below.
 int longestIncreasingSubsequence(vector<int> arr) {
@@ -20,7 +20,7 @@ int longestIncreasingSubsequence(vector<int> arr) {
             if(I[mid] < arr[i]) low = mid+1;
             else high = mid-1;
         }
-        //cout << low << " ";
+ 
         I[low] = arr[i];
         L[i] = low;
         if(low > lislen) {
@@ -28,34 +28,38 @@ int longestIncreasingSubsequence(vector<int> arr) {
         }
     }
     int pos;
-    int max = -1;
+    int mx = 0;
     for (int i = 0; i < len; i++) {
-        if(L[i] > max) {
-            max=L[i];
+        if(L[i] > mx) {
+            mx=L[i];
             pos = i;
         }
     }
-    // printing of the LIS takes place here : )
-    vector<int> ans(lislen);
-    ans.push_back(max);
-    bool vis[lislen+1] = {0};
-    vis[max] = 1;
-    max--;
-    for (int i = pos-1; i >= 0; i++) {
-        if(L[i] == max && !vis[max]) {
-            vis[max] = 1;
-            ans.push_back(max);
-            max--;
+    //cout << mx  << endl;
+
+    vector<int> ans;
+    ans.push_back(arr[pos]);
+    bool vis[mx+1] = {0};
+    vis[mx] = 1;
+    //cout << "hi";
+    //exit(0);
+    mx--;
+    for (int i = pos-1; i >= 0; i--) {
+        if(L[i] == mx && !vis[mx]) {
+            vis[mx] = 1;
+            ans.push_back(arr[i]);
+            mx--;
         }
     }
     for (int i = lislen-1; i >=0; i--) {
         cout << ans[i] << " ";
     }
-    
+    cout << endl;
+ 
     return lislen;
-
+ 
 }
-
+ 
 int main()
 {
    int n;
@@ -64,12 +68,12 @@ int main()
    for (int i = 0; i < n; i++) {
     cin >> arr[i];
    }
-
+ 
     int result = longestIncreasingSubsequence(arr);
-
+ 
     cout  << result << "\n";
-
+ 
     //fout.close();
-
+ 
     return 0;
 }
